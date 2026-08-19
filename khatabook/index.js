fb438c914a9d7bcd6bc5d927aa32eb0e1c0795e0
Param <parambedi24@gmail.com>
2026-08-19 17:35:05 +0530

    khatabook part 1

---
+++ khatabook/index.js
@@ -0,0 +1,57 @@
+const e = require("express");
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  fs.readdir("./data", (err, files) => {
+    if (err) res.send(err);
+    else {
+      res.render("index.ejs", { files: files });
+    }
+  });
+});
+
+app.get("/create", (req, res) => {
+  let today = new Date();
+  let filename = today.toISOString().split("T")[0];
+  fs.writeFile(`./data/${filename}.txt`, "", (err) => {
+    if (err) res.send(err);
+    else {
+      res.redirect("/");
+    }
+  });
+  res.send(filename);
+});
+
+app.get("/edit", (req, res) => {
+  fs.readdir("./data", (err, files) => {
+    if (err) res.send(err);
+    else {
+      let filename = req.query.filename;
+      fs.readFile(`./data/${filename}`, "utf-8", (err, data) => {
+        data = data.trim();
+        res.render("edit.ejs", { files, data, filename });
+      });
+    }
+  });
+});
+
+app.post("/update", (req, res) => {
+  let { filename, filedata } = req.body;
+  filedata = filedata.trim();
+  fs.writeFile(`./data/${filename}`, filedata, (err, data) => {
+    if (err) res.send(err);
+    else {
+      // sends back to the same url from where it started
+      res.redirect(req.get("referer"));
+    }
+  });
+});
+
+app.listen(8000, () => {
+  console.log("server is running");
+});
